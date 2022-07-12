7094d09a18a7ff07c7071f7acddf7ca3547788d4
Raman Kandratchyk <kondrator@gmail.com>
2022-07-12 23:07:50 +0300

    init: add index.js file with solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+
+  let configArr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    configArr[i] = bracketsConfig[i].join('');
+  };
+
+  let regConfig = [];
+  for (let i = 0; i < configArr.length; i++) {
+    regConfig[i] = configArr[i].replace(/\(\)|\[\]|\|\|/g, match => {
+      if (match === '()') return '\\(\\)';
+      if (match === '[]') return '\\[\\]';
+      if (match === '||') return '\\|\\|';
+    });
+  };
+
+  let reg = new RegExp(regConfig.join('|'), 'g');
+
+  let testStr = str;
+  let z;
+  do {
+    z = testStr.length;
+    testStr = testStr.replace(reg, '');
+  } while (z !== testStr.length);
+
+  return !testStr.length;
 }
